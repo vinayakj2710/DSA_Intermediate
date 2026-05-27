dfeeda8fed2e295e68d3d8fe87258c4eba9d77ab
Vinayak J <vinayak.jadhav@gmail.com>
2026-05-27 21:27:16 +0530

    Count 'ag' pairs in a given string - JAVA

---
+++ Sliding window and contribution/CarryForward.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class CarryForward{
+    public static void main(String[] args){
+        System.out.println("Carry forward technique");
+    }
+
+    private int countAGPairs(String A){
+        int N = A.length();
+        int pairs = 0;
+        int count = 0;
+        for(int i=0; i<N; i++){
+            if(A.charAt(i) == 'a'){
+                count++;
+            }
+            if(A.charAt(i) == 'g'){
+                pairs += count;
+            }
+        }
+        return pairs;
+    }
+}
+
+
+
