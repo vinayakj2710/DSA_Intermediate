e1c24f2f64d5f3028c6107a8e198c8d3fd524ffa
vinayakj2710 <vinayak.jadhav7991@gmail.com>
2026-05-28 23:56:23 +0530

    count even number in an array in a given range - java

--- Sliding window and contribution/CarryForward.java
+++ Sliding window and contribution/CarryForward.java
@@ -22,5 +22,45 @@ class CarryForward{
     }
 }
 
+//---------------- count of even numbers in given range -----------
+
+private int[] countEvenNumber(int[] A, int[][] B){
+    int N = A.length;
+    int M = B.length;
+
+    int[] pcount = new int[N];
+
+    if(A[0] % 2 == 0){
+        pcount[0] = 1;
+    }else{
+        pcount[0] = 0;
+    }
+
+    for(int i=1; i<N; i++){
+        if(A[i] % 2 == 0){
+            pcount[i] = pcount[i-1] + 1;
+        }else{
+            pcount[i] = pcount[i-1];
+        }
+    }
+
+    int[] ans = new int[M];
+
+    for(int i=0; i<m; i++){
+        int l = B[i][0];
+        int r = B[i][1];
+
+        int count = 0;
+
+        if(l == 0){
+            count = pcount[r];
+        }else{
+            count = pcount[r] - pcount[l-1];
+        }
+        ans[i] = count;
+    }
+    return ans;
+}
+
 
 
